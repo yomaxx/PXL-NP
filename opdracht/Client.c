8663c02a3f030d151e03245254eebbcd7d408d40
Max Valkenburg <max.valkenburg@hotmail.com>
2020-05-06 15:39:52 +0200

    added player turn and play again

--- opdracht/Client.c
+++ opdracht/Client.c
@@ -18,7 +18,7 @@ int main( int argc, char * argv[] )
 	sleep (1);
 
 	//check if connect failed
-	if (rd != 0)
+	if (rd != 0 && rc != 0)
     {
         printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
 
@@ -35,48 +35,71 @@ int main( int argc, char * argv[] )
     //making char for text
 	char buf [256];
 	char name[50];
-	char text[60];
+	char textJoin[60];
 	char filterPlayer[50];
-
+	char action;
 	//asking player for name
 	printf("Enter your name: ");
 	scanf("%s", name);
 
 	//parsing text to send
-	strcpy(text, berichtJoin);
-	strcat(text, name);
-	strcat(text, ">");
+	strcpy(textJoin, berichtJoin);
+	strcat(textJoin, name);
+	strcat(textJoin, ">");
 
 	//setup player filter
 	strcpy(filterPlayer, "BlackJack>game!>");
 	strcat(filterPlayer, name);
 	strcat(filterPlayer, ">");
 	printf("Filterplayer: %s\n", filterPlayer);
-	
-	//sending join with name to server
-	rd = zmq_send(publisher, text, strlen(text), 0); printf("message send\n");
-	assert (rd == strlen(text));
 
-	//waiting until server has enough players
-	printf("Wating for players\n");
+	//setup receive message
 	zmq_msg_t message;
 	rc = zmq_msg_init (&message);
-	rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterJoin, strlen (filterJoin));
-	rc = zmq_recv (subscriber, buf, 256, 0);
-    printf("Message received: %s\n\n",buf);
-    *buf = 0;
 
-	//receive first message with cards
-	rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterGame, strlen (filterGame));
-	rc = zmq_recv (subscriber, buf, 256, 0);
-    printf("Message received: %s\n\n",buf);
-    *buf = 0;
 
-    //receiving player instructions
-   	rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterPlayer, strlen (filterPlayer));
-	rc = zmq_recv (subscriber, buf, 256, 0);
-   	printf("Message received: %s\n\n",buf);
+	do{
+		//sending join with name to server
+		rd = zmq_send(publisher, textJoin, strlen(textJoin), 0); printf("message send\n");
+		assert (rd == strlen(textJoin));
 	
+		//waiting until server has enough players
+		printf("Wating for players\n");
+		rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterJoin, strlen (filterJoin));
+		rc = zmq_recv (subscriber, buf, 256, 0);
+    	printf("Message received: %s\n\n",buf);
+    	memset(buf,0,256);
+
+		//receive first message with cards
+		rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterGame, strlen (filterGame));
+		rc = zmq_recv (subscriber, buf, 256, 0);
+    	printf("Message received: %s\n\n",buf);
+    	memset(buf,0,256);
+
+    
+    	//do 	//receiving player instructions
+    	//{
+   			rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterPlayer, strlen (filterPlayer));
+			rc = zmq_recv (subscriber, buf, 256, 0);
+   			printf("action received: %s\n\n",buf);
+   			memset(buf,0,256);
+
+   			//entering action and sending
+   			printf("enter your action: ");
+			action = getchar();
+		//}while(action != 's');
+
+		//waiting for winner anouncement
+		printf("Waiting for winner anouncement...");
+		rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filterGame, strlen (filterGame));
+		rc = zmq_recv (subscriber, buf, 256, 0);
+    	printf("Message received: %s\n\n",buf);
+    	memset(buf,0,256);
+
+    	//play again
+    	printf("Do you want to play again?:");
+    	action = getchar();
+	}while(action == 'y');
 	//free (string);
 	zmq_msg_close (&message);
 	zmq_close(publisher);
